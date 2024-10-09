32b801a4e7e3d73c44a326b183d79d2ab66af982
Lucas Galdencio Barbosa <lucasgaldencio77@gmail.com>
2024-10-09 00:34:34 -0300

    Atualização Hospedes - Controller e views

--- app/controllers/hospedes_controller.rb
+++ app/controllers/hospedes_controller.rb
@@ -1,12 +1,12 @@
 class HospedesController < ApplicationController
   before_action :set_hospede, only: %i[ show edit update destroy ]
 
-  # GET /hospedes or /hospedes.json
+  # GET /hospedes
   def index
     @hospedes = Hospede.all
   end
 
-  # GET /hospedes/1 or /hospedes/1.json
+  # GET /hospedes/1
   def show
   end
 
@@ -19,52 +19,45 @@ class HospedesController < ApplicationController
   def edit
   end
 
-  # POST /hospedes or /hospedes.json
+  # POST /hospedes
   def create
     @hospede = Hospede.new(hospede_params)
 
-    respond_to do |format|
-      if @hospede.save
-        format.html { redirect_to @hospede, notice: "Hospede was successfully created." }
-        format.json { render :show, status: :created, location: @hospede }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @hospede.errors, status: :unprocessable_entity }
-      end
+    if @hospede.save
+      redirect_to @hospede, notice: "Hóspede foi criado com sucesso."
+    else
+      flash.now[:alert] = "Erro ao criar o hóspede."
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /hospedes/1 or /hospedes/1.json
+  # PATCH/PUT /hospedes/1
   def update
-    respond_to do |format|
-      if @hospede.update(hospede_params)
-        format.html { redirect_to @hospede, notice: "Hospede was successfully updated." }
-        format.json { render :show, status: :ok, location: @hospede }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @hospede.errors, status: :unprocessable_entity }
-      end
+    if @hospede.update(hospede_params)
+      redirect_to @hospede, notice: "Hóspede foi atualizado com sucesso."
+    else
+      flash.now[:alert] = "Erro ao atualizar o hóspede."
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /hospedes/1 or /hospedes/1.json
+  # DELETE /hospedes/1
   def destroy
     @hospede.destroy
-
-    respond_to do |format|
-      format.html { redirect_to hospedes_path, status: :see_other, notice: "Hospede was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to hospedes_path, status: :see_other, notice: "Hóspede foi removido com sucesso."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hospede
-      @hospede = Hospede.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def hospede_params
-      params.require(:hospede).permit(:nome, :cpf, :data_nascimento, :telefone, :email)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_hospede
+    @hospede = Hospede.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to hospedes_path, alert: "Hóspede não encontrado."
+  end
+
+  # Only allow a list of trusted parameters through.
+  def hospede_params
+    params.require(:hospede).permit(:nome, :cpf, :data_nascimento, :telefone, :email)
+  end
 end

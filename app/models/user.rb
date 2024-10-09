91873c6163a914ceb29b5ddca4489eb680b65bd1
Lucas Galdencio Barbosa <lucasgaldencio77@gmail.com>
2024-10-09 01:15:27 -0300

    Atualização relacionada a usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role: { admin: "admin", funcionario: "funcionario", candidato: "candidato", empregador: "empregador" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
